54512aa91f6162385457f388da286badbd9677f8
Daemone <kylebehm@gmail.com>
2018-09-28 19:55:35 -0400

    pkgs

--- models/MealPairData.js
+++ models/MealPairData.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const mealPairSchema = new Schema({
- 
+    wineType: {type: String, required: true},
+    wineSubTypes: [{type: String, required: true}],
+    mealPairs: [{type: String, required: true}]
 });
 
 const MealPairData = mongoose.model("MealPairData", mealPairSchema);
