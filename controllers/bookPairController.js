b62b2ce18cbf4c2208cbf9437bedd42f16d7d4dd
Daemone <kylebehm@gmail.com>
2018-09-29 00:39:20 -0400

    controller-pseudo

--- controllers/bookPairController.js
+++ controllers/bookPairController.js
@@ -1,5 +1,17 @@
 const db = require('../models');
 
+// Basic actions performed by  controller:
+// 
+//  Get/Find
+//  Update
+// 
+
 module.exports = {
+
+    // FIND ALL
+
+    // FIND ONE SUBTYPE DOCUMENT
+
+    // UPDATE ONE PROPERTY OF ONE SUBTYPE DOCUMENT
 
 };
