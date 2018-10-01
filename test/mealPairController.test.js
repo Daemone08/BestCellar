3455f5a41fc599e31fa24df525bc5081707c9cad
Daemone <kylebehm@gmail.com>
2018-09-30 23:10:20 -0400

    additional-controller-tests

--- test/mealPairController.test.js
+++ test/mealPairController.test.js
@@ -1,21 +1,21 @@
 let mongoose = require("mongoose");
 let db = require('../models');
 
-//Require the dev-dependencies
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
 let should = chai.should();
 let app = require('express')
+let mealPairsJSON = require('../scripts/seeds/mealPairs.json')
 
 chai.use(chaiHttp);
 
-//Our parent block
 describe('MealPairs Controller', () => {
 
-     describe('/GET meal pairs', () => {
+    describe('/GET meal pairs', () => {
 
-         it('it should GET all meal pairs', (done) => {
+
+        it('it should GET all meal pairs', (done) => {
             chai.request('http://localhost:3001')
                 .get('/api/mealPairs')
                 .end((err, res) => {
@@ -23,6 +23,18 @@ describe('MealPairs Controller', () => {
                     res.should.have.status(200);
                     res.body.should.be.a('array');
                     res.body.length.should.be.eql(8);
+
+                    done();
+                });
+        });
+
+        it('it should have the correct keys', (done) => {
+            chai.request('http://localhost:3001')
+                .get('/api/mealPairs')
+                .end((err, res) => {
+
+                    let testDocument = res.body[0]
+                    testDocument.should.include.keys('_id','wineType','wineSubTypes','mealPairs')
 
                     done();
                 });
